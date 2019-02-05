655160b6e9ebe0c24bcfbbc92cc393a3b7707223
Vitaly Berg <bergusman@gmail.com>
2019-02-05 23:15:33 +0800

    Fix api host

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import reducer from './reducer';
 const store = createStore(reducer);
 export default store;
 
-const ws = new WebSocket('ws://api.robodex.io');
+const ws = new WebSocket('wss://api.robodex.io');
 ws.onmessage = msg => {
   console.log(msg);
   const json = JSON.parse(msg.data);
